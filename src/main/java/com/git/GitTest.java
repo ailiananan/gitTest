4d0768dfd2942c7986da9380e4bf11cd93aa5d09
anan <593493315@qq.com>
2021-05-09 23:13:26 +0800

    push commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("bbbbbb");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
     
 }
